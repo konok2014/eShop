74d26913d3b0683e949fad34a11faea9c609c1e7
konok2014 <64254730+konok2014@users.noreply.github.com>
2020-10-20 08:56:20 +0600

    amazona v:1.0.0

---
+++ backend/data.js
@@ -0,0 +1,44 @@
+export default {
+  products: [{
+    _id:'1',
+    name: 'Slim Shirt',
+    category:'Shirts',
+    image: '/images/d1.jpg',
+    price: 60,
+    brand: 'Adidas',
+    rating: 4.5,
+    numReview: 10,
+    countInStock:6
+  },
+ {
+   _id:'2',
+    name: 'Fit Shirt',
+    category:'Shirts',
+    image: '/images/d2.jpg',
+    price: 50,
+    brand: 'Nike',
+    rating: 4.2,
+    numReview: 8,
+    countInStock:0
+  },
+ { _id:'3',
+    name: 'Best Pants',
+    category:'Shirts',
+    image: '/images/d3.jpg',
+    price: 70,
+    brand: 'Nike',
+    rating: 4.4,
+    numReview: 5,
+    countInStock:6
+  },
+  { _id:'4',
+     name: 'Best Pants',
+     category:'Shirts',
+     image: '/images/p1.jpg',
+     price: 70,
+     brand: 'Nike',
+     rating: 4.4,
+     numReview: 5,
+     countInStock:6
+   }]
+};
